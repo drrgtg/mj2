a6e886252c64424842a21c3183fada23febe0900
liushanpei <412032320@qq.com>
2020-09-24 18:26:54 +0800

    product initial

---
+++ ProductMJ2/ProductMJ2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ProductMJ2
+//
+//  Created by 刘Sir on 2020/9/24.
+//  Copyright © 2020 tutu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
